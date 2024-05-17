2dc2f9129ca25a2f75602cc75b8695c89611fc90
Swayam Terode <swayamterodex@gmail.com>
2024-05-17 19:18:38 +0530

    chore(util): add asyncHandler

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,9 @@
-const asyncHandler = (requestHandler) => (req, res, next) => {
-  Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error));
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error),
+    );
+  };
 };
 
 // const asyncHandler = (fn) => async (req, res, next) => {
