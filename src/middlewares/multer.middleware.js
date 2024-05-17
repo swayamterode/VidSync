dcec09ac5ba16257e303b3b38865ac632d220f1a
Swayam Terode <swayamterodex@gmail.com>
2024-05-17 19:20:12 +0530

    feat: multer for file upload

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,11 +2,10 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp"); // what is null? This is an error parameter. If there is no error, it should be null.
+    cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.originalname + "-" + uniqueSuffix);
+    cb(null, file.originalname);
   },
 });
 
