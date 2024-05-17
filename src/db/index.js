563bd401af5036380b08bd887bb2cc23512b8242
Swayam Terode <swayamterodex@gmail.com>
2024-05-17 19:19:41 +0530

    chore(db): update connect url

--- src/db/index.js
+++ src/db/index.js
@@ -3,9 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(
-      `${process.env.MONGO_URI}/${DB_NAME}`,
-    );
+    const connection = await mongoose.connect(`${process.env.MONGO_URI}`);
     console.log(`MongoDB Connected! DB HOST: ${connection.connection.host}`);
   } catch (error) {
     console.log("Error connecting to MongoDB.....", error);
